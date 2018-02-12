40d6ae02c64af7b71dd5dec9181d16bad6835c57
Sunshine Chong <35852811+sunschong@users.noreply.github.com>
2018-02-11 23:59:33 -0800

    Add files via upload

---
+++ TranslationSurvey.java
@@ -0,0 +1,13 @@
+package com.ejfoundation.survey;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TranslationSurvey extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_translation_survey);
+    }
+}
